d3148bcdeea7d3b9f7d52532dc2925eec1238a22
sharonkele <sharonkele98@gmail.com>
2019-11-06 16:39:08 +0000

    update models

--- application/models.py
+++ application/models.py
@@ -15,7 +15,7 @@ class Users(db.Model, UserMixin):
 	first_name = db.Column(db.String(30), nullable=False)
 	last_name = db.Column(db.String(30), nullable=False)
 	email = db.Column(db.String(150), nullable=False, unique=True)
-	password = db.Column(db.String(50), nullable=False)
+	password = db.Column(db.String(150), nullable=False)
 	lists = db.relationship('Shoppinglist', backref='creator', lazy=True)
 
 
